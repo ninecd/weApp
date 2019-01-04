b3b525ad353225948dc38a03a89260b7f117aa67
hcd <chongdong.huang@wings90.com>
2019-01-04 18:35:16 +0800

    first

--- pages/shoppingCart/shoppingCart.js
+++ pages/shoppingCart/shoppingCart.js
@@ -1,94 +1,68 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/shoppingCart/shoppingCart.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      '../../images/1.png',
-      '../../images/2.png',
-      '../../images/3.png'
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000,
-    circular: true,
-    // 内容区图片
-    conImg: [
-      '../../images/con1.png',
-      '../../images/con2.png',
-      '../../images/con3.png'
-    ]
+
   },
-  go: function () {
-    wx.navigateTo({
-      url: "",
-      success: () => {
-        console.log('success')
-      },
-      fail: () => {
-        console.log('fail')
-      }
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    wx.setNavigationBarTitle({
+      title: '购物车'
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
